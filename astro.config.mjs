de73cffac3ab2500eaeb7b66e49af8d4c91bde90
kmzk-dev <decovervoice@gmail.com>
2026-09-03 15:22:45 +0900

    Fix custom domain asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kmzk-dev.github.io',
-	base: '/fillmee',
+	site: 'https://fillmee.com',
+	base: '/',
 });
